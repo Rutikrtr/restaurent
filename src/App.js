8484d6faad7b10a4baeb8ee8614642fc1ca36558
rutikrtr <rutikshingote4325@gmail.com>
2025-05-11 21:12:44 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './components/Homepage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <Routes>
+              <Route path="/" element={<Homepage/>} />
+              {/* <Route path="/restaurant/:id" element={<RestaurantPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/registerrestaurant" element={<RegisterPageRestorant />} />
+              <Route path="/register-restaurant" element={<RestaurantRegistrationPage />} />
+              <Route path="/management" element={<ManagementPage />} /> */}
+            </Routes>
+          </Router>
   );
 }
 
